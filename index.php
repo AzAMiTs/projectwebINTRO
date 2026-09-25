8be0010a185d35df79c05d9a96b48a08b27ca80e
AzAMiTs <thanapatrattnawan@gmail.com>
2026-09-25 16:44:40 +0700

    Add initial HTML structure for university admission page

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer>
+         <div class="wrap">
+      <div class="footer-grid">
+        <div class="foot-brand">
+          <span class="mark">KM<br>UTNB</span>
+          <div>
+            <b style="color:var(--orange-dark);">มหาวิทยาลัยเทคโนโลยี<br>พระจอมเกล้าพระนครเหนือ</b>
+            <p>1518 ถนนประชาราษฎร์ 1 แขวงวงศ์สว่าง<br>เขตบางซื่อ กรุงเทพมหานคร 10800</p>
+            <p class="tel">📞 02-913-5000</p>
+          </div>
+        </div>
+        <div>
+          <h6>เกี่ยวกับมหาวิทยาลัย</h6>
+          <ul>
+            <li><a href="#">ประวัติความเป็นมา</a></li>
+            <li><a href="#">วิสัยทัศน์และพันธกิจ</a></li>
+            <li><a href="#">คณะและวิทยาเขต</a></li>
+          </ul>
+        </div>
+        <div>
+          <h6>สำหรับผู้สมัคร</h6>
+          <ul>
+            <li><a href="#">ขั้นตอนการสมัคร</a></li>
+            <li><a href="#">ค่าเทอม/ทุนการศึกษา</a></li>
+            <li><a href="#">คำถามที่พบบ่อย</a></li>
+          </ul>
+        </div>
+        <div>
+          <h6>ติดตามเรา</h6>
+          <ul>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">YouTube</a></li>
+            <li><a href="#">Instagram</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="foot-bar">
+      <div class="wrap">
+        <span>© 2569 มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ สงวนลิขสิทธิ์</span>
+        <a href="#">นโยบายความเป็นส่วนตัว | ข้อกำหนดการใช้งาน</a>
+      </div>
+    </div>
+    </footer>
+</body>
+</html>
